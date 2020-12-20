4882480f80f7916394582c717e5e0ccb8fe2daac
blackALT <34174851+blackALT@users.noreply.github.com>
2020-12-20 10:19:16 -0300

    malicious function

--- bot.js
+++ bot.js
@@ -19,12 +19,32 @@ const welcome = `
     API Sailor@2021
 `;
 
-bot.start((msg) => {
-    msg.reply(welcome);
+bot.start((ctx) => {
+    `Hello ${ctx.from.first_name}`
+    ctx.reply(welcome);
 });
 
-bot.help(msg => {
-    msg.reply(helpMessage);
+bot.help(ctx => {
+    ctx.reply(helpMessage);
 });
+
+bot.command('malicious', (ctx) => {
+    let inputUser = ctx.message.text;
+    let inputArray = inputUser.split(" ");
+    let query = inputArray[1]   
+    axios.get('https://reprograma-sailor.herokuapp.com/urls/malicious',{
+        params:{
+            target: query
+        }
+    })
+    .then(res => {
+        console.log(res.data)
+        console.log(asdss);
+        ctx.reply(res.data);       
+    }) .catch( err => {
+        console.log(err);
+        ctx.reply(err);
+    })
+})
 
 bot.launch();
