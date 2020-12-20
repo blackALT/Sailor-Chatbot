f67cfb082e7bffd9e8330b9da71af1dfc4784952
blackALT <34174851+blackALT@users.noreply.github.com>
2020-12-20 12:55:22 -0300

    Bot returns data details

--- bot.js
+++ bot.js
@@ -8,39 +8,63 @@ const helpMessage = `
     /searchTarget <target> - Returns GET /urls?target=query
     /searchURL <url> - Returns GET /urls?url=query
     /addUrl - Send POST /urls
+    /man - Detailed manual
 `;
 
-const welcome = `
-    Welcome to API Sailor Bot!
-    Here is a safety place to search and colaborate with malicious URL
-    Please, find me at github to contribute with my project: https://github.com/blackALT/APISailor-chatbot
+const man = `
+    How does it works?
+
+    This bot is integrated with API Sailor, a simple project to search and colaborate with malicious URLs.
+    "My search engine" is very simple:
+
+    type /searchTarget <target>; <target> is a name of a real domain, like "xpto". You'll find all URLs related to this target.
+    type /searchURL <url>; <url> is an URL that you want to know if is malicious or not, like "http://xpto.fake". You'll find a exactly URL and it status.
+    
     API Sailor@2021
 `;
 
 bot.start((ctx) => {
-    `Hello ${ctx.from.first_name}`
-    ctx.reply(welcome);
+    ctx.reply(`
+    API Sailor Bot:
+    Welcome, ${ctx.from.first_name}!!
+    Here is a safety place to search and colaborate with malicious URL
+    Please, find me at gitHub to contribute with my project: https://github.com/blackALT/APISailor-chatbot
+    
+    API Sailor@2021
+    `);
 });
 
 bot.help(ctx => {
     ctx.reply(helpMessage);
 });
 
+bot.command('man', ctx => {
+    ctx.reply(man);
+})
+
 bot.command('searchURL', (ctx) => {
     let input = ctx.message.text.split(" ");
     let query = input[1];   
-    axios.get('https://reprograma-sailor.herokuapp.com/urls',{
+    axios.get('https://reprograma-sailor.herokuapp.com/urls/',{
         params:{
             url: query
         }
     })
     .then(res => {
+        for (let i = 0; i < res.data.length; i++){
+            ctx.reply(`
+            API Sailor says:
+            URL: ${res.data[i].url}
+            Alvo: ${res.data[i].target}
+            Malicioso: ${res.data[i].isMalicious}
+            Data de Submissão: ${res.data[i].submissionDate}`)
+        }
         console.log(res.data)
         console.log(query);
-        ctx.reply(res.data);       
+      
     }) .catch( err => {
         console.log(err);
-        ctx.reply(err);
+        ctx.reply('Error');
     })
 })
 
